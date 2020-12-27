21f2c5b7999292b4db37feb431c573f3eeb77cab
Matt Jiang <mattljiang@gmail.com>
2020-12-27 11:58:16 -0800

    fix linting errors

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -2,67 +2,77 @@ import React, { Component } from 'react';
 import '../styles.css';
 
 class Landing extends Component {
-    constructor(props) {
-      super(props);
-      // declare state object
-      // property url and value empty str
-      this.state= {url: ''};
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    // declare state object
+    // property url and value empty str
+    this.state = { url: '' };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleSubmit(e) {
-      //send a post request to server
-      // send the selected genre in the request
-      const genreSelection = document.querySelector('#genres').value;
-      const init = {
-        method: 'POST',
-        body: JSON.stringify({genreId: genreSelection}),
-        headers: {'Content-Type': 'application/json'}
-      }
-      fetch('/startsession', init)
+  handleSubmit(e) {
+    // send a post request to server
+    // send the selected genre in the request
+    const genreSelection = document.querySelector('#genres').value;
+    const init = {
+      method: 'POST',
+      body: JSON.stringify({ genreId: genreSelection }),
+      headers: { 'Content-Type': 'application/json' },
+    };
+    fetch('/startsession', init)
       .then((response) => response.json())
       .then((data) => {
         // use setState to reassign the value of url to the received url that the server sends back
         // once setState is ran, the component renders
-        this.setState({url: data});
+        this.setState({ url: data });
       });
-    }
+  }
 
-    render() {
-     return(
-    
-       <div>
-         
-        <div >
+  render() {
+    return (
+
+      <div>
+
+        <div>
           <h1>Matchflix</h1>
-          <img id="logo" src ="https://mir-s3-cdn-cf.behance.net/project_modules/fs/f2357447098721.5873f49f96f1f.png"></img>
+          <img
+            id="logo"
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/f2357447098721.5873f49f96f1f.png"
+            alt="Matchflix logo"
+          />
         </div>
 
         <div id="startsession">
-            <label htmlFor="genres">Select a genre:</label>
-            <select name="genres" id="genres">
-              <option value="801369">Action</option>
-              <option value="4698">Animation</option>
-              <option value="48586">Children & Family</option>
-              <option value="31694">Comedy</option>
-              <option value="3979">Critically Acclaimed Films</option>
-              <option value="6839">Documentaries</option>
-              <option value="5763">Drama</option>
-              <option value="8711">Horror</option>
-              <option value="875">Indie</option>
-              <option value="1492">Sci-Fi & Fantasy</option>
-              <option value="46588">Thrillers</option>
-            </select>
-            <button id="startsession" type="button" onClick={this.handleSubmit}>Start Session</button>
-            {/* create a conditional- if the url is not an empty str, display the url to the page using a html tag */}
-            {
-            this.state.url && 
-            <p>Your url: <a href={this.state.url}>{this.state.url}</a></p>
+          <label htmlFor="genres">Select a genre:</label>
+          <select name="genres" id="genres">
+            <option value="801369">Action</option>
+            <option value="4698">Animation</option>
+            <option value="48586">Children & Family</option>
+            <option value="31694">Comedy</option>
+            <option value="3979">Critically Acclaimed Films</option>
+            <option value="6839">Documentaries</option>
+            <option value="5763">Drama</option>
+            <option value="8711">Horror</option>
+            <option value="875">Indie</option>
+            <option value="1492">Sci-Fi & Fantasy</option>
+            <option value="46588">Thrillers</option>
+          </select>
+          <button id="startsession" type="button" onClick={this.handleSubmit}>Start Session</button>
+          {/* if the url is not an empty str, display the url to the page using a html tag */}
+          {
+            this.state.url
+            && (
+            <p>
+              Your url:
+              {' '}
+              <a href={this.state.url}>{this.state.url}</a>
+            </p>
+            )
             }
         </div>
-       </div>
-     );
-    }
+      </div>
+    );
   }
+}
 
 export default Landing;
