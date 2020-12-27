0ef659bee272c6fb8a57302a525c57ed858e3bfe
Matt Jiang <mattljiang@gmail.com>
2020-12-27 11:44:58 -0800

    clean up

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -14,7 +14,6 @@ class Landing extends Component {
       //send a post request to server
       // send the selected genre in the request
       const genreSelection = document.querySelector('#genres').value;
-      console.log(genreSelection);
       const init = {
         method: 'POST',
         body: JSON.stringify({genreId: genreSelection}),
@@ -66,70 +65,4 @@ class Landing extends Component {
     }
   }
 
-
-
-  // 14:{2 items
-  //   "genre":"Critically-acclaimed Independent Films"
-  //   "netflixid":875
-  //   }
-    
-  //   29:{2 items
-  //   "genre":"Sci-Fi & Fantasy"
-  //   "netflixid":1492
-  //   }
-    
-  //   70:{2 items
-  //   "genre":"Critically Acclaimed Films"
-  //   "netflixid":3979
-  //   }
-    
-  //   82:{2 items
-  //   "genre":"Animation"
-  //   "netflixid":4698
-  //   }
-    
-  //   106:{2 items
-  //   "genre":"Drama"
-  //   "netflixid":5763
-  //   }
-    
-  //   129:{2 items
-  //   "genre":"Documentaries"
-  //   "netflixid":6839
-  //   }
-    
-  //   154:{2 items
-  //   "genre":"Horror Films"
-  //   "netflixid":8711
-  //   }
-    
-  //   452:{2 items
-  //   "genre":"Action"
-  //   "netflixid":801362
-  //   }
-    
-  //   278:{2 items
-  //   "genre":"Classic Thrillers"
-  //   "netflixid":46588
-  //   }
-    
-  //   282:{2 items
-  //   "genre":"Classic Children & Family Films"
-  //   "netflixid":48586
-  //   }
-    
-  //   262:{2 items
-  //   "genre":"Classic Comedies"
-  //   "netflixid":31694
-  //   }
-    
-
-
-
-
 export default Landing;
-
-// Landing
-    // Logo name
-        // DropDown 
-        // Start Session Button
